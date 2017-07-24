56175ce68029020c94a8b2b206d8585c3c1ea24a
Min <alstjq9292@naver.com>
2017-07-24 17:16:59 +0900

    6일차 - PHP와 JavaScript

--- phpjs/1.php
+++ phpjs/1.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
+<!--
+  php에서 출력할때는 echo 라는 문법을 사용한다.
+  php 엔진에 의해서 동작했고 그 소스의 출력된 결과를 보여주는 것이다.
+
+-->
 <html>
 <head>
   <meta charset="utf-8">
 </head>
 <body>
+  <h1>php</h1>
   <?php
-    php
+    echo 10+10;
   ?>
+  <h1>JavaScript</h1>
+  <script>
+    document.write(10+10);
+  </script>
 </body>
 </html>
