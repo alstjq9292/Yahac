0a70aa26199d0fe46d1a9c381e41d70b3b07b065
Min <alstjq9292@naver.com>
2017-07-25 14:40:45 +0900

    [7일차]프로그래밍 - 비교

---
+++ phpjs/5.php
@@ -0,0 +1,20 @@
+<!--
+  비교는 더 변수가 같은지 다른지 판단하는 것
+  boolean 이라는 데이터 형식은 true와 false 두개로 표현되고 비교의 형식이다.
+-->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script charset="utf-8">
+      document.write(1==2);
+    </script>
+    <h1>php</h1>
+    <?php
+      var_dump(1==1);
+     ?>
+  </body>
+</html>
